fb6ec8c02f5910d0ffa0ed421a90e692bef482be
Paulo Henrique da Silva Fernandes <pfernandes@braspag.com.br>
2018-09-19 16:53:32 -0300

    Corrigindo export.

--- src/CosmosDocumentDbSink.js
+++ src/CosmosDocumentDbSink.js
@@ -1,24 +1,4 @@
-const { LogEventLevel } = require(structured-log)
 const { Client, Collection } = require('documentdb-typescript')
-
-class CosmosDocumentDbSinkOptions {
-    constructor(url, authorizationKey, databaseName, collectionName, restrictedToMinimumLevel = LogEventLevel.information){
-        if(!url)
-            throw new Error(`'url' parameter is required`)
-        if(!authorizationKey)
-            throw new Error(`'authorizationKey' parameter is required`)
-        if(!databaseName)
-            throw new Error(`'databaseName' parameter is required`)
-        if(!collectionName)
-            throw new Error(`'collectionName' parameter is required`)        
-        
-        this.url = url
-        this.authorizationKey = authorizationKey
-        this.databaseName = databaseName
-        this.collectionName = collectionName
-        this.restrictedToMinimumLevel = restrictedToMinimumLevel
-    }
-}
 
 class CosmosDocumentDbSink {
     constructor(options){
@@ -43,4 +23,4 @@ class CosmosDocumentDbSink {
     }
 }
 
-module.exports = { CosmosDocumentDbSinkOptions, CosmosDocumentDbSink }
+module.exports = CosmosDocumentDbSink
